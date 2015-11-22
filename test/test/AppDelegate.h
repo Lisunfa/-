30c3af697199a632d8d163d51b18c23bf42b3584
Lisunfa <745131174qq.com>
2015-11-22 09:46:11 +0800

    test
    dads

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by apple-jd21 on 15/11/12.
+//  Copyright © 2015年 Li sunfa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by apple-jd21 on 15/11/12.
+//  Copyright © 2015年 Li sunfa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
